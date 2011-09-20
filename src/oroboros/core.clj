2a90fd9e87f523d224237d381ed5b783a6cfba9f
ryan spangler <ryan.spangler@gmail.com>
2011-09-20 01:56:10 -0700

    oroboros petals

--- src/oroboros/core.clj
+++ src/oroboros/core.clj
@@ -3,15 +3,17 @@
   (:use cantor)
   (:use oroboros.debug)
   (:use [clojure.contrib.seq-utils :only [rand-elt]])
-  (:require [penumbra.app :as app]
+  (:require [cantor.matrix :as mat]
+            [penumbra.app :as app]
             [overtone.live :as ot]
             [oroboros.sound :as sound]))
 
-(def color-scale 0.05)
+(def color-scale 1.0)
 (def vertex-scale 1.0)
 (def deviant-scales [color-scale vertex-scale])
-(def closing-factor 0.3)
-(def threshold 0.1)
+(def transform-scale 0.1)
+(def closing-factor 0.2)
+(def threshold 0.2)
 (def tones-max 5)
 
 (defn vecn
@@ -25,7 +27,7 @@
 (defn segment [c v]
   {:color c :vertex v})
 
-(defn segment-count []
+(defn pick-segment-count []
   (* 4 (+ (rand-int 42) 9)))
 ;;  (* 1 (+ (rand-int 42) 9)))
 
@@ -35,9 +37,13 @@
 (defn orbitoid [scale]
   (* scale (- (* (rand) 2) 1)))
   
+(defn random-matrix
+  ([f] (random-matrix f 1.0))
+  ([f scale] (apply mat/build-matrix (map f (repeat 16 scale)))))
+
 (defn pick-color
   ([] (pick-color 1.0))
-  ([scale] (apply vec4 (map unitoid (repeat 4 scale)))))
+  ([scale] (apply vec3 (map unitoid (repeat 3 scale)))))
 
 (defn pick-vertex
   ([] (pick-vertex 1.0))
@@ -46,6 +52,13 @@
 (defn pick-rotation
   ([] (pick-rotation 1.0))
   ([scale] (apply vec4 (map orbitoid (repeat 4 scale)))))
+
+(defn pick-color-transform
+  ([] (pick-color-transform 1.0))
+  ([scale]
+     (let [id (identity-matrix)
+           deviant (random-matrix orbitoid transform-scale)]
+       (mat/add-matrices id deviant))))
 
 (defn random-segment [& _]
   (segment (pick-color) (pick-vertex)))
@@ -76,13 +89,15 @@
 
 (defn deviate-segment
   "deviate the given segment by a certain proportion"
-  [seg scales]
-  (apply segment (map #(deviate (seg %1) %2) [:color :vertex] scales)))
+  [seg scales transform]
+  (segment (transform-vector transform (seg :color))
+           (deviate (seg :vertex) (last scales))))
+;;  (apply segment (map #(deviate (seg %1) %2) [:color :vertex] scales)))
 
 (defn add-segment
   "modify the given segment by the deviant segment"
   [seg deviant]
-  (segment (pick-color) ;; (add (seg :color) (deviant :color))
+  (segment (mix (seg :color) (deviant :color) 0.2) ;; (seg :color) ;; (pick-color) ;; (add (seg :color) (deviant :color))
            (add (seg :vertex) (deviant :vertex))))
 
 (defn mix-segments
@@ -91,31 +106,38 @@
   (segment (add (mul (a :color) (- 1.0 blend)) (mul (b :color) blend))
            (add (mul (a :vertex) (- 1.0 blend)) (mul (b :vertex) blend))))
 
-(defn segment-markers [segments trailing deviant]
+(defn segment-markers
+  "compose the structure of notable segments, mainly the head, tail, leading and trailing segments"
+  [segments trailing deviant color-transform]
   (let [next-segment (add-segment (first segments) deviant)
         last-segment (last segments)
-        deviant-segment (deviate-segment deviant deviant-scales)
+        deviant-segment (deviate-segment deviant deviant-scales color-transform)
         deviant-force (length (deviant-segment :vertex))
         cycle-vertex (normalize (sub (last-segment :vertex) (next-segment :vertex)))
         cycle-segment (segment (last-segment :color) (mul cycle-vertex deviant-force))
-        closing-segment (mix-segments deviant-segment cycle-segment closing-factor)]
+        closing-segment (mix-segments deviant-segment cycle-segment closing-factor)
+        ;; color-force (- 1.5 (length (next-segment :color)))
+;;        deviant-transform (mat/add-matrices color-transform (random-matrix unitoid (* color-force 0.1)))]
+        deviant-transform (mat/add-matrices color-transform (random-matrix orbitoid 0.1))]
     {:next-segment next-segment
      :leading-segment (first segments)
      :first-segment (first segments)
      :last-segment last-segment
      :trailing-segment trailing
      :previous-segment trailing
-     :deviant-segment closing-segment}))
+     :deviant-segment closing-segment
+     :color-transform deviant-transform}))
 
 (defn make-segments
   "build a list of n segments by starting with a random segment,
    then deviating each next from the last a small amount"
-  [n deviant]
+  [n deviant transform]
   (reduce 
-   (fn [[segments dev] _]
+   (fn [[segments dev trans] _]
      [(cons (add-segment (first segments) dev) segments)
-      (deviate-segment dev deviant-scales)])
-   [[(random-segment)] deviant]
+      (deviate-segment dev deviant-scales trans)
+      trans])
+   [[(random-segment)] deviant transform]
    (range (dec n))))
 
 (defn random-moob []
@@ -135,19 +157,30 @@
 (defn reset
   "return the application to a baseline state"
   [state]
-  (let [initial-deviant (apply merge (map #(assoc {} %1 (%2 %3)) [:color :vertex] [pick-color pick-vertex] deviant-scales))
-        [segments deviant] (make-segments (segment-count) initial-deviant)]
+  (let [segment-count (pick-segment-count)
+        deviant (apply merge
+                       (map #(assoc {} %1 (%2 %3))
+                            [:color :vertex]
+                            [pick-color pick-vertex]
+                            deviant-scales))
+        color-transform (pick-color-transform 0.1)
+        segments [(random-segment)]]
+;;        [segments deviant color-transform] (make-segments segment-count initial-deviant transform)]
     (merge
      state
      {:moobs (add-moob (state :moobs))
       :fullscreen false
+      :segment-count segment-count
       :rotation 0
+      :rotation-axis (normalize (pick-vertex))
+      :center-color (pick-color)
+      :color-transform color-transform
       :theta (vec4 0 0 0 0)
-      :dtheta (pick-rotation 20)
+      :dtheta (pick-rotation 50)
       :level 0
       :threshold (unitoid threshold)
       :segments segments}
-     (segment-markers segments (add-segment (last segments) deviant) deviant))))
+     (segment-markers segments (add-segment (last segments) deviant) deviant color-transform))))
 
 (defn make-title
   "mix the various components of oroboros together to form a modulating chain"
@@ -186,7 +219,7 @@
 (defn reshape [[x y w h] state]
   ;; (viewport 1920 1080)
   ;; (frustum-view 60.0 (/ (double w) h) -10.0 10.0)
-  (orthon 100)
+  (orthon 20)
   (merge state {:width w :height h}))
 
 (defn mouse-down [[x y] button state]
@@ -201,24 +234,39 @@
 
 (defn update [[dt t] state]
   (let [progress (/ (state :level) (state :threshold))
+        growing? (< (count (state :segments)) (state :segment-count))
         mid (merge state
          {:rotation (rem (+ (state :rotation) (* dt 10)) 360)
           :theta (add (state :theta) (mul (state :dtheta) dt))
           :level (+ (state :level) dt)
-          :leading-segment (advance-segment (state :first-segment)
-                                            (state :next-segment) progress)
-          :trailing-segment (advance-segment (state :previous-segment)
-                                             (state :last-segment) progress)
-          })
+          :leading-segment (advance-segment
+                            (state :first-segment)
+                            (state :next-segment)
+                            progress)
+          :trailing-segment (if growing?
+                              (state :previous-segment)
+                              (advance-segment
+                               (state :previous-segment)
+                               (state :last-segment)
+                               progress))})
         lead (-> mid :leading-segment :vertex)
         head (-> mid :first-segment :vertex)
+        orientation (normalize (sub lead head))
+        normal (cross lead orientation)
         trans (merge mid
-         {:orientation (normalize (sub lead head))})]
+         {:orientation orientation :normal normal})]
     
     (if (> (trans :level) (trans :threshold))
-      (let [segments (shift [(trans :next-segment)] (trans :segments))]
+      (let [segments (if growing?
+                       (cons (trans :next-segment) (trans :segments))
+                       (shift [(trans :next-segment)] (trans :segments)))]
         (merge trans {:level 0 :segments segments}
-               (segment-markers segments (trans :last-segment) (trans :deviant-segment)) deviant-scales))
+               (segment-markers segments
+                                (if growing?
+                                  (trans :previous-segment)
+                                  (trans :last-segment))
+                                (trans :deviant-segment)
+                                (trans :color-transform))))
       trans)))
 
 (defn do-segment [segment]
@@ -227,11 +275,12 @@
 
 (defn display [[dt t] state]
   ;; (apply rotate (state :theta))
-  (apply translate (sub (vec3 0 0 0) ((state :leading-segment) :vertex)))
+  (apply rotate (cons (state :rotation) (state :rotation-axis)))
+  ;; (apply translate (sub (vec3 0 0 0) ((state :leading-segment) :vertex)))
+  ;; (apply rotate (apply vec4 (cons 0 (state :normal))))
   ;; (apply rotate (cons (state :rotation) (state :orientation)))
-  ;; (apply rotate (cons (state :rotation) (state :orientation)))
-  (draw-triangle-fan
-   ;; (do-segment (segment (vec4 0 0 0 0) (vec3 0 0 0)))
+  (draw-polygon
+   (do-segment (segment (state :center-color) (vec3 0 0 0)))
    (do-segment (state :trailing-segment))
    (doall (map do-segment (reverse (state :segments))))
    (do-segment (state :leading-segment)))
